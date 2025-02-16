b5f3c7722564504d66eb2f6c8f68a711e43baa0a
Alex <alex@devarno.com>
2025-02-16 00:15:53 +0000

    staticfiles

--- unrained/settings.py
+++ unrained/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -18,6 +18,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+
+
+# Static files (CSS, JavaScript, images)
+STATIC_URL = "/static/"  # URL prefix for static files
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-!8zf28!=su$-_&^65lu2u=!d6i(3s+4zu!waov31ayrs9_7+pw'
