6b9ff7ad4839ab801f8db194999685368764e029
Alex <alex@devarno.com>
2025-03-12 14:48:07 +0000

    Fixed static files configuration

--- unrained/settings.py
+++ unrained/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-WHITENOISE_MANIFEST_STRICT = False
+# WHITENOISE_MANIFEST_STRICT = False
 
 
 # Default primary key field type
