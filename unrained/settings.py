411e82a731aecb7481ff986cd3d72b3a2337f478
Alex <alex@devarno.com>
2025-03-12 15:39:58 +0000

    staticfiles

--- unrained/settings.py
+++ unrained/settings.py
@@ -121,10 +121,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-# WHITENOISE_MANIFEST_STRICT = False
-
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
