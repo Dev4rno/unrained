163b444a2d60e40cb6054c14432c44ecb4083a33
Alex <alex@devarno.com>
2025-03-12 18:12:19 +0000

    plausible

--- unrained/settings.py
+++ unrained/settings.py
@@ -123,6 +123,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
@@ -132,3 +134,4 @@ CSRF_TRUSTED_ORIGINS = [
     "https://unrained.vercel.app",
     "https://unrained.com"
 ]
+
