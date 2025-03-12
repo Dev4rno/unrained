5c8186dd5f90d490dbbcaca9dbe41a203aac8aa1
Alex <alex@devarno.com>
2025-03-12 16:37:17 +0000

    proxy

--- weather_app/urls.py
+++ weather_app/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path("", views.city_search, name="city_search"),
     path("select/", views.city_selection, name="city_selection"),
